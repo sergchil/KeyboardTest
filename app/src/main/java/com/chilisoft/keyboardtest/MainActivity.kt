eb3ad9525191c986e4081034cd2fd66292d2e448
Chilingaryan <>
2021-12-09 18:58:43 +0400

    Initial commit

---
+++ app/src/main/java/com/chilisoft/keyboardtest/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.chilisoft.keyboardtest
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import android.view.Menu
+import android.view.MenuItem
+import com.chilisoft.keyboardtest.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonShowKeyboard.setOnClickListener {
+            showKeyboard()
+        }
+        binding.buttonHideKeyboard.setOnClickListener {
+            hideKeyboard()
+        }
+    }
+
+
+}
